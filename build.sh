e9ecf68e042bbf6628a870f7bfbbd7f4571a39f4
Robert P <robisys@users.noreply.github.com>
2017-05-31 20:23:22 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 # dotnet restore && dotnet build
 # dotnet new --info
-dotnet  --info
-dotnet  new --help
-dotnet  test --help
+# dotnet  --info
+# dotnet  new --help
+# dotnet  test --help
 # cd src/Core
 # cd src/Mail
 cd src/Api
